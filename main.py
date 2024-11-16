c2908da28997c348f5228c90fe473a03f21f1e81
Jaiditya Khemani <51130635+Marc13000@users.noreply.github.com>
2024-11-16 12:12:30 +0000

    Added some code, plus next set of data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import cryptpandas as crp
 
-decrypted_df = crp.read_encrypted(path='encrypted_data/release_3675.crypt', password='PSI9bPh4aM3iQMuE')
+decrypted_df = crp.read_encrypted(path='encrypted_data/release_3739.crypt', password='1vA9LaAZDTEKPePs')
 
 print(decrypted_df)
 
@@ -20,5 +20,7 @@ def get_submission_dict(pos_dict, your_team_name: str = "2.5 Asians", your_team_
         },
     }
 
-# #dump into csv
-# decrypted_df.to_csv('csv_data/release_3675.csv', index=False)
+# {'strat_12': 0.1, 'strat_6': -0.1, 'strat_8': 0.1, 'strat_5': 0.1, 'strat_21': 0.1, 'strat_2': -0.1, 'strat_18': -0.1, 'strat_20': -0.1, 'strat_1': -0.1, 'strat_22': 0.1, 'team_name': '2.5 Asians', 'passcode': 'tate'}
+
+# dump into csv
+decrypted_df.to_csv('csv_data/release_3739.csv', index=False)
