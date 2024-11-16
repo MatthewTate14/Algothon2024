20349179f3e919768b7b335c7e5018dd88ba7381
Jaiditya Khemani <51130635+Marc13000@users.noreply.github.com>
2024-11-16 11:47:03 +0000

    Added submission dict format

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import cryptpandas as crp
 
-decrypted_df = crp.read_encrypted(path='encrypted_data/release_3611.crypt', password='GMJVDf4WWzsV1hfL')
+decrypted_df = crp.read_encrypted(path='encrypted_data/release_3675.crypt', password='PSI9bPh4aM3iQMuE')
 
 print(decrypted_df)
 
-#dump into csv
-decrypted_df.to_csv('csv_data/release_3611.csv', index=False)
+def get_positions():
+    #TODO
+    return 0
+
+#write the function to get the submission dictionary
+def get_submission_dict(pos_dict, your_team_name: str = "2.5 Asians", your_team_passcode: str =
+"tate"):
+    return {
+        **get_positions().to_dict(),
+        **{
+            "team_name": your_team_name,
+            "passcode": your_team_passcode,
+        },
+    }
+
+# #dump into csv
+# decrypted_df.to_csv('csv_data/release_3675.csv', index=False)
